2f85e74973a45038aec85183977b4385a72a64d2
Scott Thompson <thompsons@gilt.com>
2014-02-25 00:01:05 -0500

    Add angular example code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
 console.log('This would be the main JS file.');
+
+var app = angular.module('MyDemo', ['stBlurredDialog']);
+
+app.controller('MyCtrl', ['$scope', 'stBlurredDialog', function($scope, stBlurredDialog){
+	$scope.openModal = function(){
+		stBlurredDialog.open('dialog_template.html', {msg: 'Hello from the controller!'});
+	}
+}]);
+
+app.controller('DialogCtrl', ['$scope', 'stBlurredDialog', function($scope, stBlurredDialog){
+	$scope.dialogData = stBlurredDialog.getDialogData();
+}]);
