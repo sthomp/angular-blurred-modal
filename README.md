95f38d9e86c0eb0817aaff701a765024a0052296
Scott Thompson <thompsons@gilt.com>
2014-02-24 22:46:47 -0500

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,43 @@ angular-blurred-modal
 =====================
 
 A fullscreen modal dialog that blurs the background
+
+Usage
+-----
+
+You will need to use 2 angular directives in your code. ``st-blurred-dialog-region`` specifies the region of HTML that will be blurred. ``st-blurred-dialog-overlay`` must be placed whever you want the modal HTML to render.
+
+Note that ``st-blurred-dialog-region`` is actually optional. If you just want a fullscreen modal without the blur effect then you can leave it out. (It can be useful to not have the blur effect in mobile web since the blur effect can slow down performance on some mobile browsers).
+
+Example
+-------
+
+HTML:
+
+
+```html
+<html>
+	<head>
+	</head>
+	<body>
+		<div st-blurred-dialog-region>
+			<h1>Hello World</h1>
+			<p>The body of your page goes here</p>
+		</div>
+		<st-blurred-dialog-overlay></st-blurred-dialog-overlay>
+	</body>
+</html>
+```
+
+Javascript:
+
+```javascript
+app.controller('MyController', ['$scope', 'stBlurredDialog', function($scope, stBlurredDialog){
+
+    // You can call this from your view
+    $scope.openMyDialog = function(){
+        stBlurredDialog.open(FMIConfig.Dialog.addToCollection, {data: 'This data can be passed to the modal'} );
+    }
+
+}]);
+```
---
+++ st-blurred-dialog.js
@@ -0,0 +1,92 @@
+angular.module("stBlurredDialog",[])
+	.constant('stBlurredDialogClasses',{
+		blurredRegion: 'st-blurred-region'	
+	 })
+	.factory('stBlurredDialog', ['$timeout', function($timeout){
+		var state = {
+			subscribers: [],
+			isOpen: false,
+			dialogData: null
+		}	
+
+		return {
+			open: function(pathToTemplate, data){
+				$timeout(function(){
+					state.dialogData = data;
+					state.isOpen = true;
+					angular.forEach(state.subscribers, function(subscriberCb){
+						subscriberCb(state.isOpen, pathToTemplate);
+					});
+				});
+			},
+			close: function(){
+				$timeout(function(){
+					state.isOpen = false;
+					angular.forEach(state.subscribers, function(subscriberCb){
+						subscriberCb(state.isOpen);
+					});
+				});
+			},
+			isOpen: function(){
+				return state.isOpen;
+			},
+			getDialogData: function(){
+				return state.dialogData;
+			},
+			subscribe: function(cb){
+				state.subscribers.push(cb);
+			}
+		}
+	}])
+	// This directive is used to blur the page
+	.directive('stBlurredDialogRegion', [function(){
+		return {
+			restrict: "A",
+			scope: {},
+			controller: ['$scope', 'stBlurredDialog', '$element', 'stBlurredDialogClasses', function($scope, stBlurredDialog, $element, stBlurredDialogClasses){
+
+				stBlurredDialog.subscribe(function(isOpen, path, data){
+					if(isOpen){
+						$element.addClass(stBlurredDialogClasses.blurredRegion);
+					}
+					else{
+						$element.removeClass(stBlurredDialogClasses.blurredRegion);
+					}
+				});
+
+          	}],
+			link: function(scope, element, attrs){
+			}
+		}
+	}])
+	// This directive is used to show the modal dialog
+	.directive('stBlurredDialogOverlay', [function(){
+		return {
+			restrict: "E",
+			scope: {},
+			replace: true,
+			template: 	"<div ng-if='model.isOpen' class='st-blurred-region-overlay'>" +
+						"<div style='text-align:right;'><button ng-click='close()' class='st-blurred-region-close'>&#10006;</button></div>" +
+					  	"<div ng-include src='model.pathToTemplate'></p>" +
+						"</div>",
+			controller: ['$scope', 'stBlurredDialog', '$element', function($scope, stBlurredDialog, $element){
+
+				$scope.model = {
+					// We need to bind to the state of the service to check for state changes
+					isOpen: false,
+					pathToTemplate: null
+				}
+
+				stBlurredDialog.subscribe(function(isOpen, path){
+					$scope.model.isOpen = isOpen;
+					$scope.model.pathToTemplate = path;
+				});
+				
+				$scope.close = function(){
+					stBlurredDialog.close();
+				}
+          	}],
+			link: function(scope, element, attrs){
+			}
+		}
+	}]);
